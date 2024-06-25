37655bd90e6d4c09f2c23597e08878f16e500fa0
Mariya Zakharova <zakharovamaria73@yandex.ru>
2024-06-25 14:46:19 +0400

    fix: deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { ConfigProvider } from 'antd';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { store } from './store/store';
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 root.render(
-	<BrowserRouter>
+	<HashRouter>
 		<Provider store={store}>
 			<ConfigProvider theme={theme}>
 				<App />
 			</ConfigProvider>
 		</Provider>
-	</BrowserRouter>
+	</HashRouter>
 );
